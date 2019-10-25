929b680d03db2dd8c155e6cfa3601d8217c4bb65
Francois_Gaits <francois.gaits@gmail.com>
2019-10-25 17:58:05 +0200

    début de la prise en charge de variables

--- src/Program.cpp
+++ src/Program.cpp
@@ -1,20 +1,68 @@
-#include <iostream>
-
 #include "Program.hpp"
-#include "Expr.hpp"
 
 
 Program::Program(std::istream &in) : _in{in} {};
 
-void Program::exec() const {
+bool Program::handleAssignation(std::string &s) {
+	int splitInd = s.find('=');
+
+	if(splitInd == -1) {
+		std::cerr << "'=' missing" << std::endl;
+		return false;
+	}
+	
+	std::string var = s.substr(0,splitInd);
+	std::string expr = s.substr(splitInd+1,-1);
+
+	Expr e{expr};
+	double result = e.eval();
+	std::string name = extractVariableName(var); 
+	variableMap[name] = result;
+
+	std::cout << "result (" << result << ") saved under the name '" << name << "'" << std::endl;
+
+	
+	return true;
+}
+
+std::string Program::extractVariableName(std::string &s) const {
+	std::string var;
+	std::string::const_iterator it = s.begin();
+
+	while(it != s.end()) {
+		//bypassing spaces
+		for (; it != s.end() && std::isspace(*it); ++it);
+
+		while(it != s.end() && !std::isspace(*it)) {
+			var += *it++;
+		}
+		
+		//bypassing spaces
+		for (; it != s.end() && std::isspace(*it); ++it);
+	}
+
+	return var;
+}
+
+void Program::exec() {
 	std::string s;
 	while (std::getline(_in, s) && s != "q" && s != "quit") {
 		if (!s.empty()) {
 			if (s[s.find_last_not_of(" \t\r")] == ';') {
-			} else {
+				if(!handleAssignation(s)) {
+					std::cerr << "Not a valid instruction" << std::endl;
+				}
+			} else if (std::isdigit(s.front())){
 				Expr e{s};
-				e.print();
 				std::cout << e.eval() << std::endl;
+			} else {
+				std::string name = extractVariableName(s);
+				std::cout << variableMap[name] << std::endl;
+				/*if(variableMap.find(name)) {
+					std::cout << variableMap[name] << std::endl;
+				} else {
+					std::cerr << "Not a valid instruction, maybe you forgot ';', or the variable does not exist" << std::endl;
+					}*/
 			}
 		}
 	}
--- src/Program.hpp
+++ src/Program.hpp
@@ -1,16 +1,25 @@
 #ifndef TPCPPM1S1_PROGRAM_H
 #define TPCPPM1S1_PROGRAM_H
 
-#include <istream>
+#include <iostream>
+#include <unordered_map>
+
+#include "Expr.hpp"
 
 class Program {
 private:
 	std::istream &_in;
+	std::unordered_map<std::string,double> variableMap;
 
+	bool handleAssignation(std::string &s);
+
+	std::string extractVariableName(std::string &s) const;
+	
 public:
 	explicit Program(std::istream &in);
 
-	void exec() const;
+	void exec();
+	
 };
 
 #endif //TPCPPM1S1_PROGRAM_H
